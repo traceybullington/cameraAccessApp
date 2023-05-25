77d056dac7e7c79d05d4400230d421395b1e217d
Tracey Bullington <traceylbullington@gmail.com>
2023-05-25 17:30:41 -0500

    Initial Commit

---
+++ cameraAccessApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  cameraAccessApp
+//
+//  Created by scholar on 5/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cameraAccessApp/cameraAccessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cameraAccessAppApp.swift
+//  cameraAccessApp
+//
+//  Created by scholar on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct cameraAccessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
